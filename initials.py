2be6e7a88a68cc1acc221bef05d808290cc06a12
msiaw079-blip <msiaw079@gmail.com>
2026-05-30 17:54:00 +0000

    Submission of assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Michael Yaw Siaw
+# STUDENT ID: 2425403153
+# Email: 2425403153@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Michael Yaw Siaw, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    print("print out my initials\n\n")
 
+    print('...MYS\n\n\n')
+
+
+    print('...MMMM....MMMM.......YYY..........YYY......SSSSSSSSSS.....')
+    print('...MMMM....MMMM........YYY.........YYY.....SSSSSSSSSSS.....')
+    print('...MM.MM..MM.MM.........YYY.......YYY.....SSSS.............')
+    print('...MM.MM..MM.MM..........YYY.....YYY......SSSS.............')
+    print('...MM.MM..MM.MM...........YYYY.YYYY.........SSSSSS.........')
+    print('...MM..MMMM..MM............YYYYYYY...........SSSSSS........')
+    print('...MM..MMMM..MM.............YYYYY.................SSSS.....')
+    print('...MM...MM...MM.............YYYYY.................SSSS.....')
+    print('...MM...MM...MM...**........YYYYY..**.....SSSSSSSSSSS....**')
+    print('...MM...MM...MM...**........YYYYY...**....SSSSSSSSSS.....**\n\n\n')
 
 
 main()
